1f5f881368f3f57d359c76f0ba992ca1246ada53
Triệu Phúc Hiếu <trieuphuchieu842@gmail.com>
2025-09-18 15:32:04 +0700

    first commit

---
+++ demoSpringBootThymeleaf/src/main/java/vn/iotstar/DemoSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootThymeleafApplication.class, args);
+	}
+
+}
